e2621e32d6cbbcef889a195f26308eafb2d2d3a4
mithu <nilchirkutpoison@gmail.com>
2024-05-21 13:03:46 +0600

    The child component uses the main component

---
+++ component_2/src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import Helloworld from './components/HelloWorld.vue';   // calling component
+
+const name = "Sarwar mithu";
+</script>
+
+
+<template>
+    <div>
+        <h1>Registration Form by {{name}}</h1>
+        <Helloworld />    <!--define component -->
+    </div>
+</template>
+
+
+<style scoped>
+    
+</style>
---
+++ component_2/src/components/HelloWorld.vue
@@ -0,0 +1,11 @@
+<!-- Child component -->
+<script setup>
+    const msg = "This is a new components"
+</script>
+
+
+<template>
+    <div>
+        <h1>{{ msg }}</h1>
+    </div>
+</template>
