b13e13a29f4796e14b9b3a92684808e0b006b139
mithu <nilchirkutpoison@gmail.com>
2024-05-31 09:45:43 +0600

    Attribute Binding v-bind

---
+++ attribute_V_bind_4/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+// import HelloWorld from './components/HelloWorld.vue'
+import HelloVue from './components/Hello.vue'
+
+</script>
+
+<template>
+    <HelloVue />
+</template>
+
+<style scoped>
+
+</style>
---
+++ attribute_V_bind_4/src/components/Hello.vue
@@ -0,0 +1,29 @@
+<script setup>
+
+    const mydata = "mithu";
+    const url = "https://www.linkedin.com/in/sarwar-mithu/";
+
+    const objdata = {
+        id: "sadiya",
+        href: "https://www.linkedin.com/in/sarwar-mithu/",
+    };
+
+    const attributeName = "href";
+
+</script>
+
+<template>
+    <!-- Attribute Bindings v-bind -->
+    <h1 :id="mydata">Sarwar mithu -1</h1> <br>
+    <a :href="url">Linkedin Profile</a> <br>
+
+    <h3 v-bind="objdata">call all attribute</h3> <br>
+
+    <!-- Dynamic Argument -->
+    <a v-bind:[attributeName]="url">Dynamic Argument</a>
+</template>
+
+
+<style scoped>
+
+</style>
