7a021efba20e463457b0facf3a5d1e811e421905
mithu <nilchirkutpoison@gmail.com>
2024-11-27 04:21:01 +0600

    Method using with parameter and without parameter

---
+++ method_computed_7/src/components/Hello.vue
@@ -0,0 +1,38 @@
+<!-- Method and computed -->
+<script setup>
+import { ref } from 'vue';
+
+let firstName = ref("Sadiya")
+let lastName = ref("Sarwar")
+
+// method without parameter
+// function fullName() {
+//     return firstName.value+ " "+lastName.value;
+// }
+
+// setTimeout(() => {
+//     console.log("SetTimeOut Called");
+//     lastName.value = "Mirza";
+// }, 2000);
+
+
+// method without parameter
+function fullName(dept) {                              // accepet parameter
+    return firstName.value+ " "+lastName.value +" " + "is a " + dept;
+}
+
+setTimeout(() => {                                 
+    console.log("SetTimeOut Called");
+    lastName.value = "Mirza";
+}, 2000);
+
+</script>
+
+<template>
+    <!--<h1>{{ fullName() }}</h1>-->     <!--without parameter-->
+    <h1>{{ fullName("Doctor") }}</h1>     <!--passing with parameter-->
+</template>
+
+<style scoped>
+
+</style>
---
+++ reactivity_utilities_6/src/components/Hello.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { reactive, ref, isRef } from 'vue';
+
+const a = ref(10)
+setTimeout( () =>{
+    console.log("a", a);
+    console.log("a.value", a.value)
+    a.value++;
+    console.log("is a Ref:", isRef(a));
+}, 2000);
+</script>
+
+<template>
+    <h1>Count: {{ a }}</h1>
+</template>
+
+
+<style scoped></style>
