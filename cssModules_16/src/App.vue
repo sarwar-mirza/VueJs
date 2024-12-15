06b4be3188c4d5b8234487ae50c822a4e6fa5f3c
mithu <nilchirkutpoison@gmail.com>
2024-12-15 23:46:23 +0600

    cssModule-$style.applyingCssName,Custom inject Name module

---
+++ cssModules_16/src/App.vue
@@ -0,0 +1,52 @@
+<script setup>
+import Hello from './components/Hello.vue'
+
+// Ex-03 import defaulte module
+import { useCssModule } from 'vue';
+
+
+// Ex-03 without Name
+console.log(useCssModule());
+
+// Ex-03 with Inject Name
+// console.log(useCssModule("NameInject"));
+
+
+</script>
+
+<template>
+  <Hello />
+
+  <!-- calling Ex-01 css module -->
+  <h1 :class="$style.bgColor">App Components (CSS module) </h1>
+
+  <!-- calling Ex-02 Custom inject Name -->
+   <!-- <h1 :class="NameInject.bgBlue">App Components (Custom inject Name)</h1> -->
+
+</template>
+
+<!-- Ex-01 (CSS Module)
+ A <style module> tag is compiled as css Modules and exposes the resulting CSS
+ class to the component as an object under the key of $style-->
+
+<style module>
+.bgColor{
+    background-color: blueviolet;
+}
+</style>
+
+
+
+
+<!-- Ex-02 - custom inject Name
+ syntax: module="className"
+ You can customize the property key of the injected classes object by giving the module attribute a value -->
+
+<style module="NameInject">
+.bgBlue{
+  background-color: blue;
+}
+</style>
+
+<!-- Ex -03(ex-01, 02)
+ useCssModule()- it is used to access inject class -->
---
+++ cssModules_16/src/components/Hello.vue
@@ -0,0 +1,17 @@
+<script setup>
+// v-bind in css Ex-04
+// step-01
+const theme = {textColor:"red"};
+</script>
+
+<template>
+    <h1 class="text-red">Hello Components </h1>
+</template>
+
+<style scoped>
+
+.text-red{
+    /* step-02 */
+    color: v-bind("theme.textColor");
+}
+</style>
