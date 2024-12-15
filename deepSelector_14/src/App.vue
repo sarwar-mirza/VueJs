650dabfa447528117bd20c3a4cb195bdc6014d38
mithu <nilchirkutpoison@gmail.com>
2024-12-15 15:48:38 +0600

    DeepSelector-child components you can use the deep()cls

---
+++ deepSelector_14/src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import Hello from './components/Hello.vue'
+
+</script>
+
+<template>
+  <Hello />
+</template>
+
+<style scoped>
+  /* .bg-color{
+    background-color: blueviolet;
+   } */
+
+  /* DeepSelectore */
+  .bg-color,
+  :deep(.bg-color){
+    background-color: brown;
+  }
+
+   
+</style>
---
+++ deepSelector_14/src/components/Hello.vue
@@ -0,0 +1,18 @@
+<script setup>
+</script>
+
+<template>
+    <div>
+        <h1 class="bg-color">Hello Components</h1>
+
+        <!-- Deep selector
+         if your want a selector in scoped sytles to be "deep"
+         affecting child components you can use the deep() pseudo-class -->
+        <h3 class="bg-color">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste, ex!</h3>
+    </div>
+
+    
+</template>
+
+<style scoped>
+</style>
