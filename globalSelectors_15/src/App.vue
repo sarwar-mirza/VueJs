ee812fc7c8a9f8608dccc736b4e5312cce44b23c
mithu <nilchirkutpoison@gmail.com>
2024-12-15 22:30:55 +0600

    GlobalSelector-if you want just one rule to apply globally

---
+++ globalSelectors_15/src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import Hello from './components/Hello.vue'
+
+</script>
+
+<template>
+  <Hello />
+
+  <h1 class="text-red bg-color">App Components</h1>
+</template>
+
+<style scoped>
+.bg-color{
+  background-color: blueviolet;
+}
+
+/* Global Selector
+if you want just one rule to apply globally, you can use the :global() pseudo-class rather tan creating another */
+
+:global(.text-red){
+  color: brown;
+}
+</style>
---
+++ globalSelectors_15/src/components/Hello.vue
@@ -0,0 +1,11 @@
+<script setup>
+</script>
+
+<template>
+    <h1 class="text-red">Hello Components</h1>
+
+    <h3 class="text-red">Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis nulla est, inventore blanditiis consequatur ea?</h3>
+</template>
+
+<style scoped>
+</style>
